fdbc4b766ea167021e0b41a280b16943791c5c87
Malick DIOP <diop-malick@hotmail.fr>
2018-06-28 14:48:08 +0000

    Initialize project structure

---
+++ README.md
@@ -0,0 +1,24 @@
+
+
+# Project Creation
+
+https://start.spring.io/
+
+# tech stack
+
+Java 8
+Spring Boot 2
+Spring Security
+Spring MVC
+JPA
+Thymeleaf
+MySQL
+
+Maven
+
+Bootstrap (UI Presentation)
+
+
+# links
+https://medium.com/@gustavo.ponce.ch/spring-boot-spring-mvc-spring-security-mysql-a5d8545d837d
+
---
+++ src/main/java/com/examen/GestionBanque/GestionBanque/GestionBanqueApplication.java
@@ -0,0 +1,12 @@
+package com.examen.GestionBanque.GestionBanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBanqueApplication.class, args);
+	}
+}
