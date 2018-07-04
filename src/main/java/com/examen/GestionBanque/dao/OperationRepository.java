7da5c28ccf659792e9e6e6eee47382d453e6b79a
Malick DIOP <diop-malick@hotmail.fr>
2018-07-04 00:18:40 +0000

    Lister opérations

--- src/main/java/com/examen/GestionBanque/dao/OperationRepository.java
+++ src/main/java/com/examen/GestionBanque/dao/OperationRepository.java
@@ -3,11 +3,19 @@ package com.examen.GestionBanque.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.examen.GestionBanque.entities.Operation;
 
 public interface OperationRepository extends JpaRepository<Operation, Long> {
 
-	// Page<Operation> FindByCompteIdOrderByDateOp(Long compteId, Pageable page);
+	// PropertyReferenceException: No property findByCompteNumCompte found for type
+	// Operation!
+	Page<Operation> findByCompteNumCompteOrderByDateAsc(String numCompte, Pageable pageable);
+
+	// @Query("select o from Operation o where o.compte.numCompte = ?1 order by
+	// o.date")
+	// public Page<Operation> FindByCompteOrderByDateOpQuery(String numCompte,
+	// Pageable pageable);
 
 }
