5f861cd41ccf01872047a199db7a4c1d10b83f9c
Mimi <mndiop.sne@gmail.com>
2018-07-05 15:50:35 +0000

    change role malick to Role employe

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -19,7 +19,7 @@ INSERT INTO user_role(user_id, role_name)
   VALUES
   (1, 'ROLE_ADMIN'),
   (2, 'ROLE_USER'),
-  (3, 'ROLE_USER'),
+  (3, 'ROLE_EMPLOYE'),
   (4, 'ROLE_EMPLOYE'),
   (5, 'ROLE_RESPONSABLE_COMPTE'),
   (6, 'ROLE_CAISSIERE');
