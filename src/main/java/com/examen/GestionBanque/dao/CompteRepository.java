0b409d980881aca22c731e0fc6ea3721de253c36
Malick DIOP <diop-malick@hotmail.fr>
2018-07-10 21:47:11 +0000

    virement - Liste compte destinataire

--- src/main/java/com/examen/GestionBanque/dao/CompteRepository.java
+++ src/main/java/com/examen/GestionBanque/dao/CompteRepository.java
@@ -10,5 +10,8 @@ public interface CompteRepository extends JpaRepository<Compte, String> {
 
 	// Recupérer la liste des comtpes d'un client
 	List<Compte> findByClientId(Long idClient);
+	
+	// Pour le virement, Récupèrer la liste des comptes sauf le compte d'origne
+	List<Compte> findByNumCompteIsNot(String numCompte);
 
 }
